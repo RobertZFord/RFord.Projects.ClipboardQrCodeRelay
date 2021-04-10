0d04ffd4ff570744e18688ebeffe5eaf9fc853d6
Robert Ford <RobertZFord@gmail.com>
2021-04-10 18:05:33 -0400

    feat: add output interface

---
+++ RFord.Projects.ClipboardQrCodeRelay/IOutput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RFord.Projects.ClipboardQrCodeRelay
+{
+    public interface IOutput
+    {
+        void Clear();
+        void Write(string contents);
+    }
+}
