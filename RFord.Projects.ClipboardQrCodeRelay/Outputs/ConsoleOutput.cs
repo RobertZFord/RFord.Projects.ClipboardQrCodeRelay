9dcd2c276286152e81f6a6f95e41907172f1e0dd
Robert Ford <RobertZFord@gmail.com>
2021-04-10 18:05:50 -0400

    feat: add console output implementation

---
+++ RFord.Projects.ClipboardQrCodeRelay/Outputs/ConsoleOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RFord.Projects.ClipboardQrCodeRelay.Outputs
+{
+    public class ConsoleOutput : IOutput
+    {
+        public void Clear() => Console.Clear();
+
+        public void Write(string contents) => Console.WriteLine(contents);
+    }
+}
